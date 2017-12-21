e283a6001449e83875854da9168327b72c8b6612
maxkram <max.kramer@live.com>
2017-12-21 13:59:24 -0600

    styles and UI funcs

--- app.js
+++ app.js
@@ -24,7 +24,18 @@ UI.prototype.showAlert = function(message, className){
 	const div = document.createElement("div");
 	div.className = `alert ${className}`;
 	div.appendChild(document.createTextNode(message));
-	
+	const container = document.querySelector(".container");
+	const form = document.querySelector("#book-form");
+	container.insertBefore(div,form);
+	setTimeout(function(){
+		document.querySelector(".alert").remove();
+	},3000);
+};
+
+UI.prototype.deleteBook=function(target){
+	if(target.className === "delete") {
+		target.parentElement.parentElement.remove();
+	}
 };
 
 UI.prototype.clearFields=function(){
@@ -44,12 +55,19 @@ document.getElementById("book-form").addEventListener("submit",
 		const ui = new UI();
 
 		if(title === "" || author ==="" || isbn ===""){
-			UI.showAlert("Please fill in all fields", "error");
+			ui.showAlert("Please fill in all fields", "error");
 		}else{
 			ui.addBookToList(book);
+			ui.showAlert("Book Added!", "success");
 			ui.clearFields();
 		}
 
 		e.preventDefault();
 	}
 );
+document.getElementById("book-list").addEventListener("click", function(e){
+	const ui = new UI();
+	ui.deleteBook(e.target);
+	ui.showAlert("Book Removed", "success");
+	e.preventDefault();
+});
